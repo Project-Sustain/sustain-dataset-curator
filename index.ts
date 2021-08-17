6ee88d2831baefa32568e81a1c0bee9dbed25ffe
Daniel Reynolds <danielreynolds101@gmail.com>
2021-08-17 13:38:43 -0600

    add copy type

--- index.ts
+++ index.ts
@@ -5,12 +5,20 @@ import csv = require('csv-parser');
 const { inputFilenames, joinField, outputFields, outputFile } = config;
 
 const relevantFields: Set<string> = Object.values(outputFields).reduce((acc, {input}) => {
-    for(const val of input) {
-        if(typeof val === 'string' && val.substring(0,2) !== "@@") {
-            acc.add(val);
+    if(typeof input === 'string') {
+        acc.add(input)
+    }
+    else {
+        for(const val of input) {
+            if(typeof val === 'string' && val.substring(0,2) !== "@@") {
+                acc.add(val);
+            }
         }
     }
     return acc;
 }, new Set<string>())
 
+console.log("Fields which are relevant to this curation process are: ")
 console.log(relevantFields)
+
+
